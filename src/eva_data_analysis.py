8c2b0d74024add1bea6c8505715e961c4e7f921b
Pim Nelissen <pim.nelissen@protonmail.com>
2025-02-06 11:11:47 +0100

    Update file paths

--- src/eva_data_analysis.py
+++ src/eva_data_analysis.py
@@ -111,13 +111,13 @@ def plot_cumulative_time_in_space(df, graph_file):
 if __name__ == "__main__":
 
     if len(sys.argv) < 3:
-        input_file = './eva-data.json'
-        output_file = './eva-data.csv'
+        input_file = '../data/eva-data.json'
+        output_file = '../results/eva-data.csv'
         print(f'Using default input and output filenames')
     else:
         input_file = sys.argv[1]
         output_file = sys.argv[2]
         print('Using custom input and output filenames')
 
-    graph_file = './cumulative_eva_graph.png'
+    graph_file = '../figures/cumulative_eva_graph.png'
     main(input_file, output_file, graph_file)
